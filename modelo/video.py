a076cf93b0e0fc5a27f08f7d738b2428baa99fd4
Jenypr <jenyffer.d@escolar.ifrn.edu.br>
2025-12-16 12:21:57 +0000

    projeto

--- modelo/video.py
+++ modelo/video.py
@@ -1,7 +1,7 @@
 class Video:
-def __init__(self, id=None, titulo='', url='', assistido=False, quadro_id=None):
-self.id = id
-self.titulo = titulo
-self.url = url
-self.assistido = assistido
-self.quadro_id = quadro_id
+ def __init__(self, id=None, titulo='', url='', assistido=False, quadro_id=None):
+  self.id = id
+  self.titulo = titulo
+  self.url = url
+  self.assistido = assistido
+  self.quadro_id = quadro_id
