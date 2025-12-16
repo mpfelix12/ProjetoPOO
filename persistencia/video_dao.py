92a4186adbe144523c224f9878dd2ad76e7cc5f1
Jenypr <jenyffer.d@escolar.ifrn.edu.br>
2025-12-16 12:55:28 +0000

    projeto

--- persistencia/video_dao.py
+++ persistencia/video_dao.py
@@ -1,25 +1,22 @@
-import sqlite3
 from modelo.video import Video
 
-
 class VideoDAO:
- def __init__(self, conexao):
-  self.conexao = conexao
+    def __init__(self, conexao):
+        self.conexao = conexao
 
+    def salvar(self, video: Video):
+        cursor = self.conexao.cursor()
+        cursor.execute("""
+            INSERT INTO video (titulo, url, assistido, quadro_id)
+            VALUES (?, ?, ?, ?)
+        """, (video.titulo, video.url, int(video.assistido), video.quadro_id))
+        self.conexao.commit()
+        return cursor.lastrowid
 
-def salvar(self, video: Video):
- cursor = self.conexao.cursor()
- cursor.execute(
-'INSERT INTO video (titulo, url, assistido, quadro_id) VALUES (?, ?, ?, ?)',
-(video.titulo, video.url, int(video.assistido), video.quadro_id)
-)
- self.conexao.commit()
-
-
-def buscar_por_id(self, id):
- cursor = self.conexao.cursor()
- cursor.execute('SELECT * FROM video WHERE id = ?', (id,))
- linha = cursor.fetchone()
- if linha:
-    return Video(linha[0], linha[1], linha[2], bool(linha[3]), linha[4])
- return None
+    def buscar_por_id(self, id):
+        cursor = self.conexao.cursor()
+        cursor.execute("SELECT * FROM video WHERE id = ?", (id,))
+        linha = cursor.fetchone()
+        if linha:
+            return Video(linha[0], linha[1], linha[2], bool(linha[3]), linha[4])
+        return None
