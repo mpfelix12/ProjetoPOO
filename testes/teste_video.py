6b2c19b67bf0e79344fca33ba32c0b0641191a0b
Jenypr <jenyffer.d@escolar.ifrn.edu.br>
2025-12-16 12:56:03 +0000

    projeto

--- testes/teste_video.py
+++ testes/teste_video.py
@@ -1,28 +1,35 @@
 from persistencia.database import Database
+from persistencia.canal_dao import CanalDAO
+from persistencia.quadro_dao import QuadroDAO
 from persistencia.video_dao import VideoDAO
 from modelo.video import Video
 
-
-# Criar banco e conexão
 db = Database()
 conexao = db.conexao
 
+canal_dao = CanalDAO(conexao)
+quadro_dao = QuadroDAO(conexao)
+video_dao = VideoDAO(conexao)
 
-# DAO
-dao = VideoDAO(conexao)
+# 1️⃣ Criar Canal
+canal_id = canal_dao.salvar("Canal POO")
 
+# 2️⃣ Criar Quadro
+quadro_id = quadro_dao.salvar("Quadro Aulas", canal_id)
 
-# Criar objeto Video
-video = Video(titulo='Aula POO', url='https://youtube.com/abc', assistido=False, quadro_id=1)
+# 3️⃣ Criar Vídeo
+video = Video(
+    titulo="Introdução à POO",
+    url="https://youtube.com/abc",
+    assistido=False,
+    quadro_id=quadro_id
+)
 
+video_id = video_dao.salvar(video)
 
-# Salvar no banco
-dao.salvar(video)
+# 4️⃣ Buscar Vídeo
+video_banco = video_dao.buscar_por_id(video_id)
 
-
-# Ler do banco
-video_banco = dao.buscar_por_id(1)
-
-
+print("Vídeo salvo e recuperado:")
 print(video_banco.titulo)
 print(video_banco.url)
